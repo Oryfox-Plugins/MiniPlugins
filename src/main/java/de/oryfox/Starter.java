2c2a30131d7b7de289a591bd66dacf6eec173fe9
Semih Kaiser <git@oryfox.de>
2024-01-21 22:55:47 +0100

    Starter

---
+++ src/main/java/de/oryfox/Starter.java
@@ -0,0 +1,15 @@
+package de.oryfox;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Starter extends JavaPlugin {
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
